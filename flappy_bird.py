ef7a62c398bc7884773ec5437af3314503738609
Rushaan Gandhi <rushaansps@gmail.com>
2023-05-01 09:36:44 +0530

    bird class finished

--- flappy_bird.py
+++ flappy_bird.py
@@ -57,3 +57,37 @@ class Bird:
         else:  # tilt down
             if self.tilt > -90:
                 self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+       
+        self.img_count += 1
+
+        # For animation of bird, loop through three images
+        if self.img_count <= self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count <= self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count <= self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count <= self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        #when bird is nose diving it isn't flapping
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+        
+        # tilt the bird
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_react(topLeft = (self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+    
+    def get_mask(self):
+        # """
+        # gets the mask for the current image of the bird
+        # :return: None
+        # """
+        return pygame.mask.from_surface(self.img)
