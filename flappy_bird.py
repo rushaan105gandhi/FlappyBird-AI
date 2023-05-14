99fe76d153a9381d5330803b7acac2923907a369
Rushaan Gandhi <rushaansps@gmail.com>
2023-05-15 01:43:39 +0530

    Pipe Class complete

--- flappy_bird.py
+++ flappy_bird.py
@@ -91,3 +91,81 @@ class Bird:
         # :return: None
         # """
         return pygame.mask.from_surface(self.img)
+    
+class Pipe():
+    #represents a pipe object
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.gap = 100
+
+        # where the top and bottom of the pipe is
+        self.top = 0
+        self.bottom = 0
+
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG 
+
+        self.passed = False
+
+        self.set_height()
+    
+    def set_height(self):
+        # set the height of the pipe, from the top of the screen
+        # :return: None
+        
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        # """
+        # move pipe based on vel
+        # :return: None
+        # """
+        self.x -= self.VEL
+
+    def draw(self, win):
+        
+        # draw both the top and bottom of the pipe
+        # :param win: pygame window/surface
+        # :return: None
+        # draw top
+
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+
+        # draw bottom
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird, win):
+        # """
+        # returns if a point is colliding with the pipe
+        # :param bird: Bird object
+        # :return: Bool
+        # """
+
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        top_offset = (self.x - bird.x, self.top - round(bird.y)) #how far the masks are from each other
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset) #find point of collision
+        t_point = bird_mask.overlap(top_mask,top_offset)
+
+        #check if the points exist or not
+        if b_point or t_point:
+            return True
+        
+        return False
+    
+
+
+
+    
+    
+
+
